49a1304c75c9b49a0956d02853fb5bd03a39cd2e
agungmardika <agungdmardika@gmail.com>
2024-06-14 00:00:01 +0800

    about me revision -uncomplete

--- project/project.php
+++ project/project.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> AM | Project</title>
+    <title>Agung Mardika - Project </title>
+    <link rel="shortcut icon" type="image/x-icon" href="../images/am-logo.jpg" />
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'grey' : "#202020",
-        'white': '#ffffff',
-        'purple': '#3f3cbb',
-        'midnight': '#121063',
-        'metal': '#565584',
-        'tahiti': '#3ab7bf',
-        'silver': '#ecebff',
-        'bubble-gum': '#ff77e9',
-        'bermuda': '#78dcca',
-        'olive' : '#518A51',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
       },
     },
-  }
+  },
+};
