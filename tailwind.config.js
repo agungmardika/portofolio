71492cef8857fbb0b225b6aca37c4cbe44b0cfd8
agungmardika <agungdmardika@gmail.com>
2024-06-15 02:43:18 +0800

    navbar fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+// tailwind.config.js
 module.exports = {
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      boxShadow: {
+        "inner-bottom": "inset 0 -10px 10px -5px rgba(0, 0, 0, 0.3)",
       },
     },
   },
+  plugins: [],
 };
+  
